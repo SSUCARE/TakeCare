258cd88b2b0d310496c403cafb6e302a77cf4073
Minjun Kim <damon911@naver.com>
2022-03-17 18:27:09 +0900

    minun first commit

--- app/src/main/java/com/designproject/takecare/LoginActivity.java
+++ app/src/main/java/com/designproject/takecare/LoginActivity.java
@@ -1,13 +1,14 @@
 package com.designproject.takecare;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
