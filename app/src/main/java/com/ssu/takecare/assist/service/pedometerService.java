902cc993ccdd2d589fb3be5ea002772ebbfa56aa
lhj4499 <ucs210212@naver.com>
2022-09-01 00:36:47 +0900

    만보기 서비스 class추가

---
+++ app/src/main/java/com/ssu/takecare/assist/service/pedometerService.java
@@ -0,0 +1,16 @@
+package com.ssu.takecare.assist.service;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class pedometerService extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
